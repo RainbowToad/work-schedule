562855b0ac05482ec4659d438cf7ab68f2ec78a8
RainbowToad <chad_spilker@hotmail.com>
2022-10-20 17:21:32 -0700

    added color of past/present/future

--- script.js
+++ script.js
@@ -8,11 +8,39 @@ $(document).ready(function() {
         //set items in local storage.
         localStorage.setItem(time, text);
     })
-    //load saved date to appropriate hour
-    $('.description').each(function() {
-       console.log(this);
 
-       $(this).val(localStorage.getItem("hour4"))
+    
+    var currentTime = moment().hour();
+    
+
+    //load saved data to appropriate hour
+    var hourTime = 9
+    $('.description').each(function() {
+       var hourTimeEl = $("#hour" + hourTime)
+       var hourTimetext = hourTimeEl.children().eq(1)
+      // console.log(hourTimeEl);
+        var keyHour = localStorage.getItem("hour" + hourTime)
+        hourTimetext.text(keyHour);
+        console.log(hourTime + "====" + currentTime);
+// Make Previous time=grey, present=red, future=green
+        if (currentTime<hourTime) {
+            hourTimeEl.addClass("future");
+        }else if(currentTime==hourTime) {
+            hourTimeEl.addClass("present");
+        }else{
+            hourTimeEl.addClass("past");
+        }
+
+       //$(this).val(localStorage.getItem("hour4"))
+       //Object.keys(this);
+
+
+       // loop to get each key name?
+    //    $(".description").each(function() {
+    //    var key = $(this).val(localStorage.getItem(key))
+    //    console.log(key);
+    //    })
+        hourTime++
     });
 
    
@@ -24,9 +52,7 @@ $(document).ready(function() {
     var dayEl = $('#currentDay');
     dayEl.text(moment().format("dddd MMMM Do, YYYY"));
 
-//Make previous times=grey, present=red, future=green
-    var currentTime = moment().hour();
-    console.log(currentTime);
+
 
 
 
